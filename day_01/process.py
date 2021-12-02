f287f9949809c5a7e90d0ce94a1143347c887735
Fadil Nohur <fadil.nohur@outlook.com>
2021-12-02 01:22:26 +0000

    AoC Day 1

---
+++ day_01/process.py
@@ -0,0 +1,27 @@
+import itertools
+import timeit
+
+import more_itertools
+
+
+def count_depth(depths, window=None):
+    depths = [int(depth) for depth in depths.splitlines()]
+    if window is not None:
+        depths = [sum(window) for window in more_itertools.windowed(depths, 3)]
+    increasing = sum(
+        True
+        for depth_pair in itertools.pairwise(depths)
+        if depth_pair[1] - depth_pair[0] > 0
+    )
+    return increasing
+
+
+def main():
+    with open('input.txt') as f:
+        depths = f.read()
+        print('Increasing depths:', count_depth(depths))
+        print('Increasing depths with window 3:', count_depth(depths, window=3))
+
+
+if __name__ == '__main__':
+    print(timeit.timeit(main, number=1))
