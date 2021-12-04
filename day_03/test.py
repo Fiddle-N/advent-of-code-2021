dc0cfe4ed87566b96a50294f2e4b9bab6ba9131d
Fadil Nohur <fadil.nohur@outlook.com>
2021-12-04 11:13:30 +0000

    AoC Day 3

---
+++ day_03/test.py
@@ -0,0 +1,35 @@
+from day_03 import process
+
+
+def test_power_rates():
+    report = """\
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"""
+    assert process.power_rating(report) == 198
+
+
+def test_life_support_rating():
+    report = """\
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"""
+    assert process.life_support_rating(report) == 230
