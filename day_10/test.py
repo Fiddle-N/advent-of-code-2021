2d9c329f2e0b5984b175cb3b55fd48d3a25e0402
Fadil Nohur <fadil.nohur@outlook.com>
2021-12-12 20:18:31 +0000

    AOC 2021 Day 10

---
+++ day_10/test.py
@@ -0,0 +1,33 @@
+from day_10 import process
+
+
+def test_navigation_subsystem_system_error_score():
+    code = """\
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"""
+    nav_subsystem = process.NavigationSubsystem(code)
+    assert nav_subsystem.system_error_score() == 26397
+
+
+def test_navigation_subsystem_middle_score():
+    code = """\
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"""
+    nav_subsystem = process.NavigationSubsystem(code)
+    assert nav_subsystem.middle_score() == 288957
